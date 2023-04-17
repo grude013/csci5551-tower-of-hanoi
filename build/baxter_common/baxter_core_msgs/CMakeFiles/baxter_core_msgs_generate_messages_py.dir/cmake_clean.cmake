40fa809edd27f8a4a8ad0a508682ac33dec4b3f3
shetty089 <shett089@umn.edu>
2023-04-17 19:28:35 +0000

    inital commit

---
+++ build/baxter_common/baxter_core_msgs/CMakeFiles/baxter_core_msgs_generate_messages_py.dir/cmake_clean.cmake
@@ -0,0 +1,39 @@
+file(REMOVE_RECURSE
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_AnalogIOState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_AnalogIOStates.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_AnalogOutputCommand.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_AssemblyState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_AssemblyStates.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_CameraControl.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_CameraSettings.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_CollisionAvoidanceState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_CollisionDetectionState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_DigitalIOState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_DigitalIOStates.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_DigitalOutputCommand.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_EndEffectorCommand.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_EndEffectorProperties.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_EndEffectorState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_EndpointState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_EndpointStates.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_HeadPanCommand.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_HeadState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_JointCommand.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_NavigatorState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_NavigatorStates.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_RobustControllerStatus.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_SEAJointState.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/_URDFConfiguration.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/msg/__init__.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/srv/_CloseCamera.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/srv/_ListCameras.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/srv/_OpenCamera.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/srv/_SolvePositionIK.py"
+  "/home/a/ros_ws/devel/lib/python3/dist-packages/baxter_core_msgs/srv/__init__.py"
+  "CMakeFiles/baxter_core_msgs_generate_messages_py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/baxter_core_msgs_generate_messages_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/gennodejs/ros/baxter_maintenance_msgs/msg/_index.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+let UpdateSources = require('./UpdateSources.js');
+let UpdateSource = require('./UpdateSource.js');
+let UpdateStatus = require('./UpdateStatus.js');
+let TareEnable = require('./TareEnable.js');
+let CalibrateArmData = require('./CalibrateArmData.js');
+let TareData = require('./TareData.js');
+let CalibrateArmEnable = require('./CalibrateArmEnable.js');
+
+module.exports = {
+  UpdateSources: UpdateSources,
+  UpdateSource: UpdateSource,
+  UpdateStatus: UpdateStatus,
+  TareEnable: TareEnable,
+  CalibrateArmData: CalibrateArmData,
+  TareData: TareData,
+  CalibrateArmEnable: CalibrateArmEnable,
+};
